f885023cbbbe0891358a9c942a119ee3c1386554
Gage Tseng - AWS-SL-WEB-1 <gage.tseng@geniecapital.com.tw>
2013-09-10 03:52:32 +0000

    mongos install script

---
+++ mongos_install.sh
@@ -0,0 +1,65 @@
+######### Install mongos
+#  NOTE apply 10gen.pp first
+sudo apt-key adv --keyserver keyserver.ubuntu.com --recv 7F0CEB10
+sudo apt-get update
+sudo apt-get install mongodb-10gen
+
+## Now the test file
+
+#    If the redirection operator is <<-, then all leading tab
+#    characters are stripped from input lines and the line
+#    containing delimiter.  This allows here-documents within
+#    shell scripts to be indented in a natural fashion.
+
+# the mongo init file
+cat>/etc/init/mongos.conf <<-EOF
+		# Ubuntu upstart file at /etc/init/mongos.conf
+
+		limit nofile 20000 20000
+
+		kill timeout 300 # wait 300s between SIGTERM and SIGKILL.
+
+		start on runlevel [2345]
+		stop on runlevel [06]
+
+		script
+		    exec start-stop-daemon --make-pidfile --pidfile /var/run/mongos.pid  --start --quiet --chuid mongodb --exec  /usr/bin/mongos -- --config /etc/mongos.conf; fi
+		    fi
+		end script
+EOF
+
+#the mongos conf file
+cat>/etc/mongos.conf <<-EOF
+
+	# /etc/mongos.conf
+	#mongos config file
+	 
+	#config servers
+	configdb=10.0.0.44:27019,10.0.0.45:27019,10.0.0.46:27019
+	 
+	#where to log
+	logpath=/var/log/mongodb/mongos.log
+	 
+	#log overwritten or appended to
+	logappend=true
+	 
+	#override port
+	#port=27017
+EOF
+
+# Now attach the upstart script
+ln -s /lib/init/upstart-job /etc/init.d/mongos
+
+# start on bootup
+#sudo apt-get install sysv-rc-conf
+#sysv-rc-conf mongos on
+#chkconfig -s mongos on
+#update-rc.d mongos defaults
+sudo update-rc.d -f mongos start 20 2 3 4 5 . stop 80 0 1 6 .
+# To start services use:
+# service mongos [start|stop|restart]
+
+# but down mongodb
+echo "manual" >> /etc/init/mongodb.override
+
+
