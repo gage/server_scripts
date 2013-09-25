1149670beb7ff7e8b4af1873623a562214d23737
root <ip-10-0-0-124.ec2.internal>
2013-09-25 08:43:14 +0000

    puppet hosts.pp

---
+++ hosts.pp
@@ -0,0 +1,13 @@
+host {
+  'sl-mongodb-1': ip => '10.0.0.120';
+  'sl-mongodb-2': ip => '10.0.0.121';
+  'sl-mongodb-3': ip => '10.0.0.122';
+  'sl-mongodb-4': ip => '10.0.0.124';
+  'sl-mongodb-5': ip => '10.0.0.125';
+  'sl-mongodb-6': ip => '10.0.0.126';
+  'sl-mongodb-7': ip => '10.0.0.127';
+  'sl-mongodb-8': ip => '10.0.0.128';
+  'sl-mongodb-config-1': ip => '10.0.0.140';
+  'sl-mongodb-config-2': ip => '10.0.0.141';
+  'sl-mongodb-config-3': ip => '10.0.0.142';
+}
